5fe47c0785b824eafbd3200444ba4036fdd44638
Amiras-21 <amiras09mxmx09@gmail.com>
2025-03-07 10:37:16 +0530

    fix url

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(cors());
 // app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(cors({ origin: "https://track-4-rho.vercel.app/", credentials: true }));
+app.use(cors({ 
+  origin: "https://track-4-rho.vercel.app", 
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"] }));
 
 app.use(bodyParser.json());
 // app.use(express.json());
